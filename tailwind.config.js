4484113f7a1799fd1d98879ed3a60d6d5594af7d
Rômullo Melo <81822722+rom013@users.noreply.github.com>
2023-05-02 17:47:07 -0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.jsx"],
+  theme: {
+    extend: {},
+    // colors:{
+    //   "white-background" : "#F5F7FB",
+    //   "primary" : "#22B77E",
+    // }
+  },
+  plugins: [],
+}
+
