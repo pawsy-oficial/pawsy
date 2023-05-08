2c6f8b0219c502e0ee4b281d24e359bdec490239
Enzo3322 <enzo.spag14@gmail.com>
2023-05-08 20:21:44 -0300

    refactor: ensure header component returns active state when match url pathname

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,88 +1,131 @@
-import { Question, SignOut } from "@phosphor-icons/react"
-import Logo from "../img/logo.png"
+import { Question, SignOut } from "@phosphor-icons/react";
+import Logo from "../img/logo.png";
 
-export function NavbarTutor(props){
-    const pages = [
-        "Início",
-        "Carteira de vacinação",
-        "Agendar consulta",
-        "Vét. mais próximo",
-        "Bem-estar",
-        "Receitas",
-    ]
+export function NavbarTutor() {
+  const pages = [
+    {
+      name: "Inicío",
+      pathname: "/clinic",
+    },
+    {
+      name: "Carteira de vacinação",
+      pathname: "/tutor",
+    },
+    {
+      name: "Agendar consulta",
+      pathname: "/consulta",
+    },
+    {
+      name: "Vét. mais próximo",
+      pathname: "/vets",
+    },
+    {
+      name: "Bem-estar",
+      pathname: "/bem-estar",
+    },
+    {
+      name: "Receitas",
+      pathname: "/receitas",
+    },
+  ];
 
-    return(
-        <section className="bg-primary h-full min-h-screen sticky top-0 px-6 py-10 flex flex-col gap-16 justify-between">
-            <div className="w-52">
-                <img src={Logo} alt="Logotipo PAWSY"/>
-            </div>
-            
-            <nav className="flex flex-col w-[calc(100%+24px)] gap-4">
-                <ul>
-                    {
-                        pages.map((page, index) => {
-                            return(
-                                <li
-                                    key={index}
-                                    className={`nav-link ${index == props.page && "bg-[#F5F7FB] !text-black f after:block before:block"}`}
-                                >
-                                    <a href="#" >
-                                        {page}
-                                    </a>
-                                </li>
-                            )
-                        })
-                    }
-                </ul>
-            </nav>
-            <div className="flex justify-between text-white">
-                <span>
-                    <SignOut size={24} color="#fff"/>
-                </span>
-                <span>
-                    <Question size={24} color="#fff"/>
-                </span>
-            </div>
-        </section>
-    )
+  const isActive = (pathname) => {
+    return window?.location?.pathname === pathname;
+  };
+
+  return (
+    <section className="bg-primary h-full min-h-screen sticky top-0 px-6 py-10 flex flex-col gap-16 justify-between">
+      <div className="w-52">
+        <img src={Logo} alt="Logotipo PAWSY" />
+      </div>
+
+      <nav className="flex flex-col w-[calc(100%+24px)] gap-4">
+        <ul>
+          {pages.map((page, index) => {
+            return (
+              <li
+                key={index}
+                className={`nav-link ${
+                  isActive(page.pathname)
+                    ? "bg-[#F5F7FB] !text-black f after:block before:block"
+                    : ""
+                }`}
+              >
+                <a href={page.pathname}>{page.name}</a>
+              </li>
+            );
+          })}
+        </ul>
+      </nav>
+      <div className="flex justify-between text-white">
+        <span>
+          <SignOut size={24} color="#fff" />
+        </span>
+        <span>
+          <Question size={24} color="#fff" />
+        </span>
+      </div>
+    </section>
+  );
 }
 
-export function NavbarClinic(props){
-    const pages = [
-        "Inicío",
-        "Agenda",
-        "Marketing",
-        "Pacientes",
-        "Perfil",
-    ]
+export function NavbarClinic() {
+  const pages = [
+    {
+      name: "Inicío",
+      pathname: "/clinica",
+    },
+    {
+      name: "Agenda",
+      pathname: "/agenda",
+    },
+    {
+      name: "Marketing",
+      pathname: "/marketing",
+    },
+    {
+      name: "Pacientes",
+      pathname: "/pacientes",
+    },
+    {
+      name: "Perfil",
+      pathname: "/perfil",
+    },
+  ];
 
+  const isActive = (pathname) => {
+    return window?.location?.pathname === pathname;
+  };
 
-    return(
-        <section className="bg-primary h-full min-h-screen sticky top-0 px-6 py-10 flex flex-col gap-16 justify-between">
-            <div className="w-52">
-                <img src={Logo} />
-            </div>
-            <nav className="flex flex-col w-[calc(100%+24px)] gap-4">
-                {
-                    pages.map((page, index) => {
-                        return(
-                            <li 
-                                className={`nav-link ${index == props.page && "bg-[#F5F7FB] !text-black f after:block before:block"}`}
-                            >
-                                {page}
-                            </li>
-                        )
-                    })
-                }
-            </nav>
-            <div className="flex justify-between text-white">
-                <span>
-                    <SignOut size={24} color="#fff"/>
-                </span>
-                <span>
-                    <Question size={24} color="#fff"/>
-                </span>
-            </div>
-        </section>
-    )
+  return (
+    <section className="bg-primary h-full min-h-screen sticky top-0 px-6 py-10 flex flex-col gap-16 justify-between">
+      <div className="w-52">
+        <img src={Logo} />
+      </div>
+      <nav className="flex flex-col w-[calc(100%+24px)] gap-4">
+        {pages.map((page, index) => {
+          return (
+            <li
+              key={index}
+              className={`nav-link ${
+                isActive(page.pathname)
+                  ? "bg-[#F5F7FB] !text-black f after:block before:block"
+                  : ""
+              }`}
+            >
+              <a href={page.pathname}>{page.name}</a>
+            </li>
+          );
+        })}
+      </nav>
+      <div className="flex justify-between text-white">
+        <span>
+          <SignOut size={24} color="#fff" />
+        </span>
+        <span>
+          <Question size={24} color="#fff" />
+        </span>
+      </div>
+    </section>
+  );
 }
--- src/pages/tutor/Tutor.jsx
+++ src/pages/tutor/Tutor.jsx
@@ -1,13 +1,13 @@
 import { Header } from "../../components/header/Header";
 import { NavbarTutor } from "../../components/Navbar";
 
-export default function Tutor(){
-    return(
-        <main className="flex min-h-screen">
-            <NavbarTutor page={0}/>
-            <section className="flex-1">
-                <Header/>
-            </section>
-        </main>
-    )
+export default function Tutor() {
+  return (
+    <main className="flex min-h-screen">
+      <NavbarTutor />
+      <section className="flex-1">
+        <Header />
+      </section>
+    </main>
+  );
 }
