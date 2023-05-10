08d628f8c2dd2f7ade9c628051aa154496c75653
Fernando-Fernandes05 <fer.fernandes.moraes05@gmail.com>
2023-05-10 16:04:07 -0300

    forth-commit2

--- src/Router.jsx
+++ src/Router.jsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom"
 import Tutor from "./pages/tutor/Tutor"
 import Clinic from "./pages/clinic/Clinic"
 import VetCloser from "./pages/tutor/VetCloser"
+import Revenues from "./pages/tutor/Revenues"
 
 export default function Router(){
     return(
@@ -9,6 +10,7 @@ export default function Router(){
             <Route path="/tutor" element={<Tutor/>} />
             <Route path="/clinica" element={<Clinic/>} />
             <Route path="/vets" element={<VetCloser/>} />
+            <Route path="/receitas" element={<Revenues/>} />
         </Routes>
     )
 }
--- src/pages/tutor/Revenues.jsx
+++ src/pages/tutor/Revenues.jsx
@@ -1,18 +1,16 @@
 import { Header } from "../../components/header/Header";
 import { NavbarTutor } from "../../components/Navbar";
-import "./index.css"
 
-export default function VetCloser() {
+export default function Revenues() {
     return (
       <main className="flex min-h-screen">
         <NavbarTutor />
         <section className="flex-1">
           <Header/>
           <main>
-            <h1>Veterinário mais próximo de você:</h1>
+            <h1>Receitas médicas do</h1>
           </main>
         </section>
       </main>
     );
   }
-  
