62a9c95b08d6fb41d6a432f45fa692b8b1a140ef
rom013 <romullomelo0130@gmail.com>
2023-06-06 13:26:04 -0300

    feat: add more veterinary button

--- src/pages/clinic/Schedule.jsx
+++ src/pages/clinic/Schedule.jsx
@@ -1,25 +1,23 @@
-import { PlusCircle } from "@phosphor-icons/react";
+import { CaretLeft, PlusCircle, Trash } from "@phosphor-icons/react";
 import Calendar from "../../components/componentsClinic/calendar/Calendar";
 import ContainerSchedule from "../../components/componentsClinic/containerSchedule/ContainerSchedule";
 import { Header } from "../../components/header/Header";
 import { NavbarClinic } from "../../components/Navbar";
 import { useEffect, useState } from "react";
-import * as Select from "@radix-ui/react-select";
+import { InputDropDown } from "../../components/componentsClinic/Input";
 
 const veterinaryNameDataBase = ["Vanessa Santos", "Leonardo Nabio", "Thereza Soares"]
 
 
 export default function Schedule() {
     const [pageContent, setPageContent] = useState(0)
-    const content = [HomeSchedule, FormNewSchedule]
-
     function PageSelect({ page }) {
 
         switch (pageContent) {
             case 0:
                 return <HomeSchedule />
             case 1:
-                return <FormNewSchedule />
+                return <FormNewSchedule alterPage={setPageContent}/>
         }
     }
 
@@ -32,18 +30,21 @@ export default function Schedule() {
                 <section className="p-8">
                     <div className="flex gap-5">
                         <section className="flex-1 max-w-5xl">
-                            <PageSelect page={pageContent} />
+                            <PageSelect page={pageContent}/>
                         </section>
                         <article className="w-[348px] flex flex-col items-center gap-6">
                             <Calendar page={setPageContent} />
 
-                            <div
-                                className="bg-[#04AD34] w-full rounded-lg px-6 py-2 gap-4 flex items-center cursor-pointer"
-                                onClick={() => setPageContent(1)}
+                            <button
+                                className="bg-[#04AD34] w-full rounded-lg px-6 py-2 gap-4 flex items-center cursor-pointer disabled:cursor-default disabled:opacity-25"
+                                onClick={() => {
+                                    setPageContent(1)
+                                }}
+                                disabled={pageContent != 0 && true}
                             >
                                 <PlusCircle color="#fff" size={24} weight="bold" />
                                 <p className="text-white">Criar nova agenda</p>
-                            </div>
+                            </button>
 
                             <div className="text-base self-start" onClick={() => setPageContent(0)}>Ver todas agendas</div>
                         </article>
@@ -65,16 +66,34 @@ function HomeSchedule() {
     )
 }
 
-function FormNewSchedule() {
+function FormNewSchedule({alterPage}) {
     const [veterinaryName, setVeterinaryName] = useState([])
     useEffect(() => {
         setVeterinaryName(veterinaryNameDataBase)
     }, [])
 
     const [valueTextArea, setValueTextArea] = useState("")
+    const [sectionAddVeterinary, setSectionAddVeterinary] = useState([""])
+
+    function heandleAddSection(){
+        setSectionAddVeterinary([...sectionAddVeterinary, ""])
+    }
+
+    function heandleRemoveSection(){
+        const remove = [...sectionAddVeterinary]
+        remove.pop()
+        setSectionAddVeterinary(remove)
+    }
 
     return (
         <div className="max-w-2xl">
+            <div
+                className="flex gap-2 items-center cursor-pointer"
+                onClick={()=>{alterPage(0)}}
+            >
+                <CaretLeft/>
+                <span>Voltar</span>
+            </div>
             <h1 className="font-sora font-bold text-[32px]">Nova agenda</h1>
 
             <TitleSectionForm title={"Período da agenda"} description={"Informe o período em que a agenda ficará disponível para agendamento."} />
@@ -102,16 +121,20 @@ function FormNewSchedule() {
                 </div>
             </section>
 
-
             <TitleSectionForm title={"Veterinário"} description={"Adicione o nome do veterinário, sua especialidade e os dias e horários disponíveis para atendimento."} />
 
             <div className="flex flex-col gap-6">
-
-                <SectionAddVeterinary names={veterinaryName}/>
-
+                {
+                    sectionAddVeterinary.map(() => {
+                        return <SectionAddVeterinary names={veterinaryName}/>
+                    })
+                }
+                {
+                    sectionAddVeterinary.length == 0 && <span className="text-zinc-400 text-center">Adicione um médico veterinário para sua nova consulta</span>
+                }
                 <div className="w-full flex justify-center gap-6 text-primary font-lato font-bold">
-                    <span className="cursor-pointer">adicionar mais</span>
-                    <span className="cursor-pointer">remover</span>
+                    <span className="cursor-pointer" onClick={heandleAddSection}>adicionar mais</span>
+                    <span className="cursor-pointer" onClick={heandleRemoveSection}>remover</span>
                 </div>
             </div>
 
@@ -147,11 +170,13 @@ function TitleSectionForm({ title, description }) {
 }
 
 function SectionAddVeterinary({names}) {
+    const week = ["Domingo", "Segunda-feira", "Terça-feira", "Quarta-feira", "Quinta-feira", "Sexta-feira", "sábado"]
+
     return (
-        <section className="border border-zinc-500 rounded-tr-2xl rounded-bl-2xl rounded-tl-lg rounded-br-lg p-3 flex flex-col gap-3 w-full">
+        <section className="border border-zinc-500 rounded-tr-2xl rounded-bl-2xl rounded-tl-lg rounded-br-lg p-3 flex flex-col gap-3 w-full group">
             <div className="flex justify-center gap-6">
-                <div className="flex flex-col gap-1">
-                    <strong className="text-base font-lato font-normal">Veterinario</strong>
+                <label className="flex flex-col gap-1">
+                    <strong className="text-base font-lato font-normal">Veterinário</strong>
                     <select name="" id="" className="py-1 px-6 rounded-lg border border-zinc-300 focus:border-primary min-w-[256px] focus:outline-primary">
                         {
                             names.map(e => {
@@ -163,21 +188,18 @@ function SectionAddVeterinary({names}) {
                             })
                         }
                     </select>
-                </div>
-                <div className="flex flex-col gap-1">
+                </label>
+                <label className="flex flex-col gap-1">
                     <strong className="text-base font-lato font-normal">Especialidade</strong>
                     <input type="text" className="py-1 px-6 rounded-lg border border-zinc-300 focus:border-primary min-w-[256px]" placeholder="__:__" />
-                </div>
+                </label>
             </div>
             <div className="flex justify-center gap-6">
-                <div className="flex flex-col gap-1">
-                    <strong className="text-base font-lato font-normal">Dias disponives</strong>
-                    <input type="text" className="py-1 px-6 rounded-lg border border-zinc-300 focus:border-primary min-w-[256px]" placeholder="__/__/____" />
-                </div>
-                <div className="flex flex-col gap-1">
-                    <strong className="text-base font-lato font-normal">Horario disponivel</strong>
+                <InputDropDown listData={week} />
+                <label className="flex flex-col gap-1">
+                    <strong className="text-base font-lato font-normal">Horario disponível</strong>
                     <input type="text" className="py-1 px-6 rounded-lg border border-zinc-300 focus:border-primary min-w-[256px]" placeholder="__:__" />
-                </div>
+                </label>
             </div>
         </section>
     )
