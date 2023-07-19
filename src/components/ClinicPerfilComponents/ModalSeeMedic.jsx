ee08295d102df47b74488390ef017da8cb2f74ad
PedroDanielBrunetto <pedrodanielbm@hotmail.com>
2023-07-19 16:47:38 -0300

    create-patient

--- src/components/ClinicPerfilComponents/ModalSeeMedic.jsx
+++ src/components/ClinicPerfilComponents/ModalSeeMedic.jsx
@@ -8,8 +8,8 @@ export function ModalSeeMedic(props){
     return(
       <div className="fixed top-0 bottom-0 left-0 right-0 bg-[#111111]/60">
         <div className=" fixed top-2/4 left-1/2 -translate-x-2/4 -translate-y-2/4 bg-white rounded-lg">
-          <div className='flex justify-end p-1'><button onClick={() => props.setSee(!props.isSee)}><X size={28}/></button></div>
-          <div className='flex p-10 items-center'>
+          <div className='flex justify-end pr-1 pt-1'><button onClick={() => props.setSee(!props.isSee)}><X size={28}/></button></div>
+          <div className='flex p-6 items-center'>
           <div>
               <img src={pessoa} alt={pessoa} className="flex items-center justify-center w-44 h-44 border border-primary rounded-full" />
           </div>
--- src/pages/clinic/Patients.jsx
+++ src/pages/clinic/Patients.jsx
@@ -5,12 +5,13 @@ export default function Patient(){
     return(
         <main className="flex min-h-screen">
             <NavbarClinic page={0}/>
-
             <section className="flex-1">
                 <Header/>
+
                 <main className="pl-10 pr-16 py-8 flex gap-5 justify-center items-center">
                     <h2>🚧 Em desenvolvimento 🚧</h2>
                 </main>
+                
             </section>
         </main>
     )
