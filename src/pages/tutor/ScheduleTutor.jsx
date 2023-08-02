f9ccb4bde86ad09cdb9529457ca13fb512191034
rom013 <romullomelo0130@gmail.com>
2023-08-02 12:41:24 -0300

    fix: style ajust

--- src/pages/tutor/ScheduleTutor.jsx
+++ src/pages/tutor/ScheduleTutor.jsx
@@ -6,69 +6,7 @@ import { Header } from "../../components/header/Header";
 import { useState } from "react";
 import ScheduleNotFound from "../../components/scheduleNotFound";
 
-export default function ScheduleTutor() {
-    useTopToScreen()
-    const [handlePage, setHandlePage] = useState(false)
-
-    return (
-        <main className="flex min-h-screen">
-            <NavbarTutor page={2} />
-            <section className="flex-1">
-                <Header userType={"tutor"} />
-
-                {
-                    handlePage && (
-                        <a
-                            className="flex items-center ml-10 text-sm cursor-pointer my-5 group"
-                            onClick={() => setHandlePage(false)}
-                        >
-                            <CaretLeft className="group-hover:text-primary" />
-                            Voltar
-                        </a>
-                    )
-                }
-
-                <main className={`mx-6 lg:ml-10 px-3 py-5 md:p-10 rounded-lg bg-white mb-16 shadow-md lg:max-w-6xl ${!handlePage && "mt-10"}`}>
-
-                    {
-                        handlePage
-                            ? <NewSchedule /> // true 
-                            : <MySchedules alterNewSchedulePage={setHandlePage} /> // false
-                    }
-
-                </main>
-            </section>
-        </main>
-    )
-}
-
-function MySchedules({ alterNewSchedulePage }) {
-    return (
-        <>
-            <strong className="text-2xl font-lato font-semibold">Minhas agendas</strong>
-
-            <section className="flex flex-col sm:grid sm:grid-cols-2 lg:grid-cols-3 w-full gap-6 mt-4 mb-10">
-                <CardSchedule deleteButton={true} clinicName={"fulano PET"} scheduleDate={"2023-07-28"} scheduleHour={"13:00"} vetName={"Yuri Bernadi"} scheduleType={"consulta"} logoVet={"https://placehold.co/600x400"} />
-                <CardSchedule deleteButton={true} clinicName={"fulano PET"} scheduleDate={"2023-07-28"} scheduleHour={"13:00"} vetName={"Yuri Bernadi"} scheduleType={"consulta"} logoVet={"https://placehold.co/600x400"} />
-                <CardSchedule deleteButton={true} clinicName={"fulano PET"} scheduleDate={"2023-07-28"} scheduleHour={"13:00"} vetName={"Yuri Bernadi"} scheduleType={"consulta"} logoVet={"https://placehold.co/600x400"} />
-                <CardSchedule deleteButton={true} clinicName={"fulano PET"} scheduleDate={"2023-07-28"} scheduleHour={"13:00"} vetName={"Yuri Bernadi"} scheduleType={"consulta"} logoVet={"https://placehold.co/600x400"} />
-                <CardSchedule deleteButton={true} clinicName={"fulano PET"} scheduleDate={"2023-07-28"} scheduleHour={"13:00"} vetName={"Yuri Bernadi"} scheduleType={"consulta"} logoVet={"https://placehold.co/600x400"} />
-                <CardSchedule deleteButton={true} clinicName={"fulano PET"} scheduleDate={"2023-07-28"} scheduleHour={"13:00"} vetName={"Yuri Bernadi"} scheduleType={"consulta"} logoVet={"https://placehold.co/600x400"} />
-                <CardSchedule deleteButton={true} clinicName={"fulano PET"} scheduleDate={"2023-07-28"} scheduleHour={"13:00"} vetName={"Yuri Bernadi"} scheduleType={"consulta"} logoVet={"https://placehold.co/600x400"} />
-            </section>
-
-            <button
-                className="flex gap-3 items-center text-primary"
-                onClick={() => { alterNewSchedulePage(true) }}
-            >
-                <PlusCircle size={24} weight="bold" className="fill-primary" />
-                <span className="font-bold">agendar uma nova consulta</span>
-            </button>
-        </>
-    )
-}
-
-const schedules =
+const SCHEDULES =
     [
         {
             date: "2023-07-01",
@@ -152,23 +90,86 @@ const schedules =
         }
     ]
 
+export default function ScheduleTutor() {
+    useTopToScreen()
+    const [handlePage, setHandlePage] = useState(false)
+
+    return (
+        <main className="flex min-h-screen">
+            <NavbarTutor page={2} />
+            <section className="flex-1">
+                <Header userType={"tutor"} />
+
+                {
+                    handlePage && (
+                        <a
+                            className="flex items-center ml-10 text-sm cursor-pointer my-5 group"
+                            onClick={() => setHandlePage(false)}
+                        >
+                            <CaretLeft className="group-hover:text-primary" />
+                            Voltar
+                        </a>
+                    )
+                }
+
+                <main className={`mx-6 lg:ml-10 px-3 py-5 md:p-10 rounded-lg bg-white mb-16 shadow-md lg:max-w-6xl ${!handlePage && "mt-10"}`}>
+
+                    {
+                        handlePage
+                            ? <NewSchedule /> // true 
+                            : <MySchedules alterNewSchedulePage={setHandlePage} /> // false
+                    }
+
+                </main>
+            </section>
+        </main>
+    )
+}
+
+function MySchedules({ alterNewSchedulePage }) {
+    return (
+        <>
+            <strong className="text-2xl font-lato font-semibold">Minhas agendas</strong>
+
+            <section className="flex flex-col sm:grid sm:grid-cols-2 lg:grid-cols-3 w-full gap-6 mt-4 mb-10">
+                <CardSchedule deleteButton={true} clinicName={"fulano PET"} scheduleDate={"2023-07-28"} scheduleHour={"13:00"} vetName={"Yuri Bernadi"} scheduleType={"consulta"} logoVet={"https://placehold.co/600x400"} />
+                <CardSchedule deleteButton={true} clinicName={"fulano PET"} scheduleDate={"2023-07-28"} scheduleHour={"13:00"} vetName={"Yuri Bernadi"} scheduleType={"consulta"} logoVet={"https://placehold.co/600x400"} />
+                <CardSchedule deleteButton={true} clinicName={"fulano PET"} scheduleDate={"2023-07-28"} scheduleHour={"13:00"} vetName={"Yuri Bernadi"} scheduleType={"consulta"} logoVet={"https://placehold.co/600x400"} />
+                <CardSchedule deleteButton={true} clinicName={"fulano PET"} scheduleDate={"2023-07-28"} scheduleHour={"13:00"} vetName={"Yuri Bernadi"} scheduleType={"consulta"} logoVet={"https://placehold.co/600x400"} />
+                <CardSchedule deleteButton={true} clinicName={"fulano PET"} scheduleDate={"2023-07-28"} scheduleHour={"13:00"} vetName={"Yuri Bernadi"} scheduleType={"consulta"} logoVet={"https://placehold.co/600x400"} />
+                <CardSchedule deleteButton={true} clinicName={"fulano PET"} scheduleDate={"2023-07-28"} scheduleHour={"13:00"} vetName={"Yuri Bernadi"} scheduleType={"consulta"} logoVet={"https://placehold.co/600x400"} />
+                <CardSchedule deleteButton={true} clinicName={"fulano PET"} scheduleDate={"2023-07-28"} scheduleHour={"13:00"} vetName={"Yuri Bernadi"} scheduleType={"consulta"} logoVet={"https://placehold.co/600x400"} />
+            </section>
+
+            <button
+                className="flex gap-3 items-center text-primary hover:bg-primary/20 p-1 rounded-lg"
+                onClick={() => { alterNewSchedulePage(true) }}
+            >
+                <PlusCircle size={24} weight="bold" className="fill-primary" />
+                <span className="font-bold">agendar uma nova consulta</span>
+            </button>
+        </>
+    )
+}
+
+
+
 
 function NewSchedule() {
     const date = new Date()
     const dayCurrent = date.getDate()
-    const monthCurrent = (date.getMonth()+1).toString().padStart(2,0)
+    const monthCurrent = (date.getMonth() + 1).toString().padStart(2, 0)
     const yearCurrent = date.getFullYear()
 
     const [handleSwitch, setHandleSwitch] = useState(false)
     const [dateCurrent, setDateCurrent] = useState(`${yearCurrent}-${monthCurrent}-${dayCurrent}`)
     const [typeSchedule, setTypeSchedule] = useState("consulta")
 
-
     function handleFilterSchedule() {
-        const filterSchedule = schedules.filter(schedule => schedule.date == date)
+        const filterSchedule = SCHEDULES.filter(schedule => schedule.date == dateCurrent)
 
         return filterSchedule.length < 1
-            ? <ScheduleNotFound mesage={"Infelizmente, não há horários disponíveis para a data selecionada. Por favor, escolha outra data."}/>
+            ? <ScheduleNotFound mesage={"Infelizmente, não há horários disponíveis para a data selecionada. Por favor, escolha outra data."} />
             : filterSchedule.map((scheduleFilter) => {
                 const filterTypeSchedule = scheduleFilter.schedules.filter(a => a.type == typeSchedule)
                 return filterTypeSchedule.length < 1
@@ -202,7 +203,7 @@ function NewSchedule() {
                             type="date"
                             id="dateSchedule"
                             disabled={handleSwitch}
-                            onBlur={(e) => { setDateCurrent(e.target.value) }}
+                            onChange={(e) => { setDateCurrent(e.target.value) }}
                             value={dateCurrent}
                             className="border-2 disabled:opacity-20 transition-all border-primary rounded px-1 bg-[#F5FFFE]"
                         />
@@ -211,11 +212,14 @@ function NewSchedule() {
                         <label className="text-base" htmlFor="selectTypeSchedule">Tipo do agendamento:</label>
                         <select
                             id="selectTypeSchedule"
-                            className="md:w-40 px-2 md:px-6 border border-primary rounded bg-[#F5FFFE] focus:border-2 active:outline-none focus-visible:outline-none"
+                            className="px-2 md:px-6 border border-primary rounded bg-[#F5FFFE] focus:border-2 active:outline-none focus-visible:outline-none"
                             onChange={(e) => setTypeSchedule(e.target.value)}
                         >
                             <option value="consulta">Consulta</option>
-                            <option value="exame">Exame</option>
+                            <option value="internacao">Internação</option>
+                            <option value="ultrassom">Ultrassom</option>
+                            <option value="eletrocardiograma">Eletrocardiograma</option>
+                            <option value="cirurgia">Cirurgia</option>
                         </select>
                     </div>
                 </div>
@@ -232,13 +236,16 @@ function NewSchedule() {
                     {
                         handleSwitch
                             ? (
-                                schedules.map(schedule => {
+                                SCHEDULES.map(schedule => {
                                     return (
                                         schedule.schedules.filter((schedule) => {
                                             return schedule.type == typeSchedule
                                         }).map(e => {
                                             return (
-                                                <CardSchedule clinicName={e.clinicName} logoVet={e.clinicLogo} scheduleDate={schedule.date} scheduleHour={e.hour} scheduleType={e.type} vetName={e.veterinaryName} />
+                                                <>
+                                                    {/* <CardSchedule /> */}
+                                                    <CreateNewScheduleModal clinicName={e.clinicName} logoVet={e.clinicLogo} scheduleDate={schedule.date} scheduleHour={e.hour} scheduleType={e.type} vetName={e.veterinaryName} />
+                                                </>
                                             )
                                         })
                                     )
@@ -255,9 +262,53 @@ function NewSchedule() {
     )
 }
 
+import * as AlertDialog from '@radix-ui/react-alert-dialog';
+
+function CreateNewScheduleModal({ clinicName,logoVet, scheduleDate, scheduleHour, scheduleType, vetName }) {
+    return (
+        <AlertDialog.Root>
+            <AlertDialog.Trigger>
+                <CardSchedule clinicName={clinicName} logoVet={logoVet} scheduleDate={scheduleDate} scheduleHour={scheduleHour} scheduleType={scheduleType} vetName={vetName} />
+            </AlertDialog.Trigger>
+            <AlertDialog.Portal>
+                <AlertDialog.Overlay className="bg-primary/50 z-[60] flex justify-center items-center fixed inset-0" />
+                <AlertDialog.Content className="fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-[60] bg-white py-4 px-16 rounded-lg">
+                    <AlertDialog.Description className="text-mauve11 mt-4 mb-5 text-[15px] leading-normal">
+                        <section className="flex flex-col gap-2 items-center mb-2">
+                            <figure className="w-20 h-20">
+                                <img src={logoVet} alt={`Logo ${clinicName}`} className="object-cover h-full w-full"/>
+                            </figure>
+                            <span className="font-bold text-lg">{ clinicName }</span>
+                            <small className="text-zinc-500">R. xxxxxxx, 550</small>
+                        </section>
+                        <hr />
+                        <section className="mt-4 flex flex-col gap-6 items-center">
+                            <span className="underline text-primary text-base">{ vetName }</span>
+                            <span>{ scheduleDate } { scheduleHour }</span>
+                            <strong className="font-bold text-2xl uppercase">caramelo</strong>
+                        </section>
+                    </AlertDialog.Description>
+                    <div className="flex justify-end gap-6 mt-6">
+                        <AlertDialog.Cancel asChild>
+                            <button className="hover:bg-red-error hover:text-white transition-all rounded py-1 px-4 text-black border border-red-error">
+                                Cancelar
+                            </button>
+                        </AlertDialog.Cancel>
+                        <AlertDialog.Action asChild>
+                            <button className="bg-green-600 rounded py-1 px-4 text-white">
+                                Agendar
+                            </button>
+                        </AlertDialog.Action>
+                    </div>
+                </AlertDialog.Content>
+            </AlertDialog.Portal>
+        </AlertDialog.Root>
+    )
+}
+
 function CardSchedule({ deleteButton, logoVet, clinicName, scheduleDate, scheduleHour, scheduleType, vetName }) {
     return (
-        <div className={`flex relative gap-2 shadow-md rounded-lg p-3 sm:w-full ${!deleteButton && "cursor-pointer"} bg-[#f8fffc]`}>
+        <div className={`flex relative gap-6 shadow-md rounded-lg p-3 sm:w-full ${!deleteButton && "cursor-pointer"} bg-[#f8fffc]`}>
             {
                 deleteButton && (
                     <button className="absolute right-2 top-2">
@@ -270,17 +321,18 @@ function CardSchedule({ deleteButton, logoVet, clinicName, scheduleDate, schedul
             </div>
 
             <div className="flex flex-col gap-3">
-                <strong className="text-base">{clinicName}</strong>
-                <div className="flex flex-col gap-2">
+                <strong className="text-base text-start">{clinicName}</strong>
+                <div className="flex flex-col gap-2 items-start">
                     <div className="flex gap-3 text-sm">
                         <span>{scheduleDate}</span>
                         <span className="font-bold">{scheduleHour}</span>
                     </div>
-                    <span className="font-bold text-sm">{scheduleType}</span>
+                    <span className="font-bold text-sm capitalize">{scheduleType}</span>
 
-                    <p className="text-sm text-primary underline">{vetName}</p>
+                    <p className="text-sm text-primary underline capitalize">{vetName}</p>
                 </div>
             </div>
         </div>
     )
 }
+
