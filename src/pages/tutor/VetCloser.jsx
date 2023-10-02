115c102ded747aec837b214000c43806c1adb8dc
rom013 <romullomelo0130@gmail.com>
2023-10-01 23:08:01 -0300

    add data at infoBox map

--- src/pages/tutor/VetCloser.jsx
+++ src/pages/tutor/VetCloser.jsx
@@ -112,34 +112,9 @@ export default function VetCloser() {
 	}, [location.latitude, location.longitude]);
 
 	let map, pinLayer, searchPolygon, infobox;
+	var infoboxLayer = new Microsoft.Maps.EntityCollection();
 
-	let infoboxTemplate = `
-                <div class="p-3 bg-white rounded-lg flex flex-col w-60 shadow-lg">
-                    <div class="flex gap-3 items-center">
-                        <div class="bg-red-400 rounded-full w-10 h-10"></div>
-                        <div class="flex flex-col">
-                            <h3 class="text-2xl font-lato font-bold">{title}</h3>
-                            <span class="text-green-600 text-xs">aberto</span>
-                        </div>
-                    </div>
-                    <div class="flex flex-col">
-                        <p class="text-zinc-400 text-xs my-2">{address}</p>
-                        <div class="flex justify-between w-full items-center">
-                            <span class="text-base font-semibold">1.4KM</span>    
-                            <div class="flex gap-4 items-center">
-                                <span class="text-xs">4,8</span>    
-                                <div class="flex gap-1">
-									<div class="bg-yellow-500 w-2 h-2"></div>   
-									<div class="bg-yellow-500 w-2 h-2"></div>   
-									<div class="bg-yellow-500 w-2 h-2"></div>   
-									<div class="bg-yellow-500 w-2 h-2"></div>   
-									<div class="bg-yellow-500 w-2 h-2"></div>   
-                                </div>    
-                            </div>    
-                        </div>
-                    </div>
-                </div>
-		`
+	let executed = false
 
 	function GetMap() {
 		// console.log(location.latitude, location.longitude);
@@ -157,7 +132,7 @@ export default function VetCloser() {
 			// zoom map
 			zoom: 15,
 			minZoom: 14,
-			maxZoom: 15,
+			maxZoom: 16,
 
 			navigationBarMode: navigationBarMode.square, // estilo dos controles
 			supportedMapTypes: [Microsoft.Maps.MapTypeId.road, Microsoft.Maps.MapTypeId.aerial], // estilo dos controles
@@ -185,62 +160,51 @@ export default function VetCloser() {
 		pinLayer = new Microsoft.Maps.Layer();
 		map.layers.insert(pinLayer);
 
-		clinicsLocation.forEach((location) => {
-			/*function getDistanceBetweenPoints(latitude1, longitude1, latitude2, longitude2, unit = 'kilometers') {
-				let theta = longitude1 - longitude2;
-				let distance = 60 * 1.1515 * (180/Math.PI) * Math.acos(
-					Math.sin(latitude1 * (Math.PI/180)) * Math.sin(latitude2 * (Math.PI/180)) + 
-					Math.cos(latitude1 * (Math.PI/180)) * Math.cos(latitude2 * (Math.PI/180)) * Math.cos(theta * (Math.PI/180))
-				);
-				if (unit == 'miles') {
-					return Math.round(distance, 2);
-				} else if (unit == 'kilometers') {
-					return Math.round(distance * 1.609344, 2);
-				}
-			}*/
-			insertPin(location)
-		})
+		if(!executed && clinicsLocation.length){
+		executed = true
+			clinicsLocation.forEach((location) => {
+				insertPin(location)
+			})
+		}
+		infobox = new Microsoft.Maps.Infobox(new Microsoft.Maps.Location(0, 0), { visible: false });
+		infoboxLayer.push(infobox);
 
 
-		const title = "Test"
-		const address = `Praça coronel lopez`
-
-		infobox = new Microsoft.Maps.Infobox({ latitude: location.latitude, longitude: location.longitude }, {
-			visible: false,
-			htmlContent: infoboxTemplate.replace('{title}', title).replace('{address}', address)
-		});
 
 		//Assign the infobox to a map instance.
 		infobox.setMap(map);
 	}
 
 	function Search() {
-		console.log("aqui");
-		console.log(searchExecuted);
 		//Use the center of the map as the center of the search area.
 		let origin = { latitude: location.latitude, longitude: location.longitude };
-		let radiusOption = 2.5;
+		let radiusOption = 1;
 		let radius = parseFloat(radiusOption);
 
 		//Get all the pushpins from the pinLayer.
 		let pins = pinLayer.getPrimitives();
 
 		//Loop through each pushpin and calculate its distance from the origin and change the color depending on if it is within the search area or not.
-		for (let i = 0; i < pins.length; i++) {
-			// console.log(typeof Microsoft.Maps.SpatialMath.getDistanceTo);
-			let distance = Microsoft.Maps.SpatialMath.getDistanceTo(origin, pins[i].getLocation(), Microsoft.Maps.SpatialMath.DistanceUnits.Kilometers);
 
-			if (distance <= radius) {
-				pins[i].setOptions({ color: 'blue' });
-				// console.log(pins[i].metadata.id);
-				const idClinic = pins[i].metadata.id
-				setClinicCloser(oldArray => [...oldArray, idClinic])
-				// const newArrayIdClinic = [...clinicCloser, idClinic]
-				// console.log(newArrayIdClinic);
-			} else {
-				pins[i].setOptions({ visible: false });
+		if(pins.length > 0){
+			for (let i = 0; i < pins.length; i++) {
+				// console.log(typeof Microsoft.Maps.SpatialMath.getDistanceTo);
+				let distance = Microsoft.Maps.SpatialMath.getDistanceTo(origin, pins[i].getLocation(), Microsoft.Maps.SpatialMath.DistanceUnits.Kilometers);
+				// console.log("oook");
+	
+				if (distance <= radius) {
+					pins[i].setOptions({ color: 'blue' });
+					// console.log(pins[i].metadata.id);
+					const idClinic = pins[i].metadata.id
+					setClinicCloser(oldArray => [...oldArray, idClinic])
+					// const newArrayIdClinic = [...clinicCloser, idClinic]
+					// console.log(newArrayIdClinic);
+				} else {
+					pins[i].setOptions({ visible: false });
+				}
 			}
 		}
+
 
 		//Create a circle polygon to show the search area. 
 		let circleLocs = Microsoft.Maps.SpatialMath.getRegularPolygon(origin, radius, 36, Microsoft.Maps.SpatialMath.DistanceUnits.Kilometers);
@@ -257,47 +221,97 @@ export default function VetCloser() {
 		}
 	}
 
+	
+
 	function insertPin({ latitude, longitude, idClinica }) {
 		// console.log(answer.results[0].location); // jogar isso no banco
-		let c = new Microsoft.Maps.Pushpin(
-			{ latitude: latitude, longitude: longitude }, {
-			icon: `<svg width="29" height="39" viewBox="0 0 29 39" fill="none" xmlns="http://www.w3.org/2000/svg"><g clip-path="url(#clip0_1583_2688)"><path d="M28.75 16.42C28.75 27.97 14.5 38.75 14.5 38.75C14.5 38.75 0.25 27.97 0.25 16.42C0.25 12.1314 1.75133 8.01855 4.42373 4.98608C7.09612 1.95362 10.7207 0.25 14.5 0.25C18.2793 0.25 21.9039 1.95362 24.5763 4.98608C27.2487 8.01855 28.75 12.1314 28.75 16.42Z" fill="#1BA8C4"/><path d="M14.5 23.75C19.6086 23.75 23.75 19.6086 23.75 14.5C23.75 9.39137 19.6086 5.25 14.5 5.25C9.39137 5.25 5.25 9.39137 5.25 14.5C5.25 19.6086 9.39137 23.75 14.5 23.75Z" fill="#F8F8F8" stroke="#F8F8F8" stroke-width="0.75" stroke-linecap="round" stroke-linejoin="round"/><path d="M14.6284 9.27149C14.3001 9.35733 13.6078 9.77094 13.6424 11.1798C13.646 11.3262 13.8305 12.5141 14.8841 12.5929C15.6743 12.652 16.1336 11.6681 16.0992 10.7569C16.0662 9.88155 15.3722 9.18637 14.7174 9.25463C14.6924 9.25723 14.6624 9.26261 14.6284 9.27149ZM11.1467 10.1614C11.0564 10.1855 10.9718 10.222 10.8955 10.2714C10.6748 10.4145 10.1009 11.0935 10.9157 12.4462C10.9925 12.5736 11.752 13.5381 12.72 13.1385C13.4459 12.8389 13.3561 11.7675 12.8679 10.9799C12.4649 10.3298 11.6986 10.0141 11.1467 10.1614ZM17.4616 11.262C17.1135 11.3536 16.5327 11.7171 16.3871 12.8941C16.3702 13.0305 16.3784 14.1562 17.3516 14.3564C18.0815 14.5065 18.6465 13.6479 18.7403 12.7974C18.8305 11.9803 18.2783 11.2509 17.6573 11.2354C17.6099 11.2342 17.542 11.2409 17.4616 11.262ZM9.88659 13.3781C9.72807 13.4165 9.58769 13.4878 9.47917 13.5924C9.30261 13.7625 8.90155 14.4884 9.90889 15.5966C10.0037 15.7009 10.8867 16.4603 11.7041 15.9243C12.3171 15.5224 12.0318 14.5488 11.4326 13.906C11.0009 13.4429 10.3621 13.2628 9.88659 13.3781ZM14.2568 14.0805C12.9442 14.4176 12.5797 15.3493 12.3408 16.0731C12.0354 16.9984 11.8144 18.243 12.5425 18.5863C13.5326 19.0533 13.9277 18.4036 14.448 18.097C15.1027 17.7113 15.2956 17.6373 15.6268 17.5657C16.0722 17.4694 16.5003 17.5788 16.9283 17.623C18.1947 17.7539 18.496 17.2818 18.466 16.7894C18.4004 15.7125 17.0661 14.9415 16.2496 14.4291C15.6228 14.0358 14.9845 13.9075 14.32 14.0649C14.2987 14.0699 14.2776 14.0751 14.2568 14.0805Z" fill="black"/></g><defs><clipPath id="clip0_1583_2688"><rect width="28.5" height="38.5" fill="white" transform="translate(0.25 0.25)"/></clipPath></defs></svg>`
-		});
-
-		c.metadata = {
-			title: 'Pin Title',
-			description: 'Pin discription',
-			id: idClinica
-		};
-		Microsoft.Maps.Events.addHandler(c, "click", pushpinClicked)
-
-		pinLayer.add(c)
+		
+		axios.get(`${import.meta.env.VITE_URL}/ClinicPreviews?id=${idClinica}`)
+			.then(res => {
+				let c = new Microsoft.Maps.Pushpin(
+					{ latitude: latitude, longitude: longitude }, {
+					icon: `
+						<svg width="37" height="49" viewBox="0 0 37 49" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M13.5 42.5C15.5 44.1667 19 47 24 42.5M9 43C11 44.6667 19 52.5 28 43" stroke="#1BA8C4"/><g filter="url(#filter0_d_1622_2746)"><path d="M33 18.4537C33 30.2063 18.5 41.1754 18.5 41.1754C18.5 41.1754 4 30.2063 4 18.4537C4 14.0899 5.52767 9.90484 8.24695 6.81917C10.9662 3.73351 14.6544 2 18.5 2C22.3456 2 26.0338 3.73351 28.753 6.81917C31.4723 9.90484 33 14.0899 33 18.4537Z" fill="#1BA8C4"/></g><path d="M18.5002 25.9123C23.6984 25.9123 27.9125 21.6983 27.9125 16.5C27.9125 11.3017 23.6984 7.08772 18.5002 7.08772C13.3019 7.08772 9.08789 11.3017 9.08789 16.5C9.08789 21.6983 13.3019 25.9123 18.5002 25.9123Z" fill="#F8F8F8" stroke="#F8F8F8" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/><path d="M18.6305 11.1798C18.2964 11.2671 17.592 11.688 17.6271 13.1215C17.6308 13.2705 17.8186 14.4793 18.8907 14.5595C19.6947 14.6196 20.1621 13.6184 20.1271 12.6912C20.0935 11.8005 19.3873 11.0931 18.7211 11.1626C18.6956 11.1653 18.6651 11.1707 18.6305 11.1798ZM15.0877 12.0853C14.9958 12.1098 14.9097 12.1469 14.8321 12.1972C14.6075 12.3428 14.0236 13.0337 14.8527 14.4102C14.9308 14.5398 15.7036 15.5212 16.6886 15.1146C17.4272 14.8097 17.3358 13.7195 16.8391 12.9182C16.429 12.2566 15.6493 11.9354 15.0877 12.0853ZM21.5134 13.2052C21.1592 13.2984 20.5682 13.6683 20.42 14.8659C20.4029 15.0048 20.4112 16.1502 21.4015 16.3538C22.1441 16.5066 22.719 15.6329 22.8145 14.7675C22.9063 13.9361 22.3444 13.1939 21.7125 13.1781C21.6643 13.1769 21.5952 13.1837 21.5134 13.2052ZM13.8055 15.3584C13.6442 15.3975 13.5013 15.47 13.3909 15.5764C13.2112 15.7496 12.8031 16.4882 13.8282 17.6158C13.9247 17.722 14.8232 18.4947 15.6549 17.9493C16.2786 17.5403 15.9883 16.5497 15.3786 15.8956C14.9393 15.4243 14.2893 15.2411 13.8055 15.3584ZM18.2523 16.0731C16.9167 16.4162 16.5458 17.3642 16.3027 18.1007C15.992 19.0422 15.7671 20.3086 16.5079 20.658C17.5155 21.1332 17.9174 20.472 18.4469 20.1601C19.1131 19.7677 19.3094 19.6923 19.6464 19.6195C20.0996 19.5215 20.5352 19.6328 20.9707 19.6778C22.2593 19.811 22.566 19.3306 22.5354 18.8295C22.4687 17.7337 21.111 16.9493 20.2801 16.4279C19.6423 16.0277 18.9928 15.8971 18.3167 16.0572C18.295 16.0623 18.2735 16.0676 18.2523 16.0731Z" fill="black"/><defs><filter id="filter0_d_1622_2746" x="0" y="0" width="37" height="47.1754" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB"><feFlood flood-opacity="0" result="BackgroundImageFix"/><feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha"/><feOffset dy="2"/><feGaussianBlur stdDeviation="2"/><feComposite in2="hardAlpha" operator="out"/><feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"/><feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_1622_2746"/><feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_1622_2746" result="shape"/></filter></defs></svg>
+					`
+				});
+		
+				c.metadata = {
+					nameClinic: res.data.Nome,
+					address: res.data.Endereço,
+					image: res.data.Imagem,
+					id: idClinica
+				};
+				Microsoft.Maps.Events.addHandler(c, "click", pushpinClicked)
+		
+				pinLayer.add(c)
+			})
+			.catch(err => console.log(err))
 
 		Microsoft.Maps.loadModule('Microsoft.Maps.SpatialMath');
 	}
 
 	function pushpinClicked(e) {
-		//Make sure the infobox has metadata to display.
-		console.log(e);
-		if (e.target.metadata) {
-			//Set the infobox options with the metadata of the pushpin.
+		if (e.targetType == "pushpin") {
+			var pin = e.target;
+			console.log(pin.metadata);
+			var html = 
+			`
+				<div class="p-3 bg-white rounded-lg flex flex-col w-60 shadow-lg">
+					<div class="flex gap-3 items-center">
+						<div class="bg-primary overflow-hidden rounded-full w-10 h-10">
+							<img src="${import.meta.env.VITE_URL}/files/${pin.metadata.image}" class="h-full w-full object-cover">
+						</div>
+						<div class="flex flex-col">
+							<h3 class="text-2xl font-lato font-bold">
+								${pin.metadata.nameClinic}
+							</h3>
+							<span class="text-green-600 text-xs">aberto</span>
+						</div>
+					</div>
+					<div class="flex flex-col">
+						<p class="text-zinc-400 text-xs my-2">
+							${pin.metadata.address}
+						</p>
+						<div class="flex justify-between w-full items-center">
+							<span class="text-base font-semibold">1.4KM</span>    
+							<div class="flex gap-4 items-center">
+								<span class="text-xs">4,8</span>    
+								<div class="flex gap-1">
+									<div class="bg-yellow-500 w-2 h-2"></div>   
+									<div class="bg-yellow-500 w-2 h-2"></div>   
+									<div class="bg-yellow-500 w-2 h-2"></div>   
+									<div class="bg-yellow-500 w-2 h-2"></div>   
+									<div class="bg-yellow-500 w-2 h-2"></div>   
+								</div>    
+							</div>    
+						</div>
+					</div>
+				</div>
+			`;
+
 			infobox.setOptions({
-				location: e.target.getLocation(),
-				title: e.target.metadata.title,
-				description: e.target.metadata.description,
-				visible: true
+				visible: true,
+				offset: new Microsoft.Maps.Point(-33, 20),
+				htmlContent: html
 			});
+
+			//set location of infobox
+			infobox.setLocation(pin.getLocation());
 		}
 	}
 
-	const [ clinicInfoPreview, setClinicInfoPreview ] = useState([])
-	useEffect(()=>{
-		clinicCloser.forEach(id=>{
+	const [clinicInfoPreview, setClinicInfoPreview] = useState([])
+	useEffect(() => {
+		clinicCloser.forEach(id => {
+			console.log("1");
 			axios.get(`${import.meta.env.VITE_URL}/ClinicPreviews?id=${id}`)
-				.then(res => setClinicInfoPreview(oldInfos => [...oldInfos, res.data]))
+			.then(res => {
+					console.log("ddd");
+					setClinicInfoPreview(oldInfos => [...oldInfos, res.data])
+				})
 				.catch(err => console.log(err))
 		})
-	},[clinicCloser])
+	}, [clinicCloser])
 
 	return (
 		<main className="flex min-h-screen">
@@ -326,7 +340,7 @@ export default function VetCloser() {
 						<div className="flex flex-row gap-4 overflow-x-auto pb-3 mt-6">
 							{
 								clinicInfoPreview.map(
-									clinicInfo => <CardsVetCloser nameClinic={clinicInfo.Nome} clinicOpenOrClose={"Aberto"} address={clinicInfo.Endereço} distanceFromTheClinic={"1.5 km"}  img={clinicInfo.Imagem} assessment={"4,0"} id={clinicInfo.Id} />
+									clinicInfo => <CardsVetCloser nameClinic={clinicInfo.Nome} clinicOpenOrClose={"Aberto"} address={clinicInfo.Endereço} distanceFromTheClinic={"1.5 km"} img={clinicInfo.Imagem} assessment={"4,0"} id={clinicInfo.Id} />
 								)
 							}
 						</div>
