41550867d0f12138786d7dd970605bcb64ecdd89
rom013 <romullomelo0130@gmail.com>
2023-10-05 11:37:22 -0300

    feat: connect database clinic page

--- src/pages/clinic/Perfil.jsx
+++ src/pages/clinic/Perfil.jsx
@@ -14,12 +14,16 @@ import gato from '../../img/gato.jpg'
 import caramelo from '../../img/caramelo.jpg'
 
 import { PlusCircle } from "@phosphor-icons/react";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import Cookies from "js-cookie";
 
 
 export default function Perfil() {
     const [open, setOpen] = useState(false)
     const [see, setSee] = useState(false)
+    const [infoClinic, setInfoClinic] = useState([])
+
 
     const clinica = [
         { nameClinic: "Petz", imageClinic: "https://pbs.twimg.com/profile_images/1489579803846090758/PI8ujLgX_400x400.jpg" }
@@ -27,6 +31,25 @@ export default function Perfil() {
     const infos = [
         { endereco: "Rua José Lobo Viana, 173", telefone: "(13)3203-3766", email: "petzco@petz.com", funcionamento: "Das 7:00 ás 21:00" }
     ]
+
+    let tokenClinic
+
+    useEffect(() => {
+        tokenClinic = Cookies.get("jwtTokenClinic")
+
+        axios.get(`${import.meta.env.VITE_URL}/profileClinic`, {
+            headers: {
+                Authorization: `Bearer ${tokenClinic}`
+            }
+        })
+            .then(e => {
+                setInfoClinic(e.data);
+            })
+            .catch(err => {
+                console.log(err)
+            })
+    }, [])
+
     return (
         <main className="flex min-h-screen">
             <NavbarClinic page={3} />
@@ -41,23 +64,30 @@ export default function Perfil() {
                 </main>
                 <main className="pl-10 pr-16 py-8 flex gap-5">
                     <section className="flex-1 flex flex-col bg-white px-6 py-8 rounded-2xl">
-                        <section className="flex justify-between">
+                        <section className="flex justify-between relative">
                             <div className="flex items-center">
-                                <div className="w-48 h-48 rounded-lg overflow-hidden">
-                                    <img src={clinica[0].imageClinic} alt={clinica[0].nameClinic} className="h-full w-full object-cover" />
+                                <div className="min-w-[12rem] w-48 h-48 rounded-lg overflow-hidden">
+                                    <img
+                                        src={`${import.meta.env.VITE_URL}/files/${infoClinic.storedImg}`}
+                                        alt={infoClinic.storedNameClinica}
+                                        className="h-full w-full object-cover"
+                                    />
                                 </div>
                                 <div className="flex flex-col p-4 gap-2  text-left">
                                     <h3 className="text-[32px] font-bold uppercase flex gap-4 items-center">
-                                        {clinica[0].nameClinic}
+                                        {infoClinic.storedNameClinica}
                                     </h3>
                                     <p>
-                                        {infos[0].endereco}
+                                        {infoClinic.Rua}, {infoClinic.Numero}
+                                    </p>
+                                    {
+                                        infoClinic.Complemento && <p>{infoClinic.Complemento}</p>
+                                    }
+                                    <p>
+                                        {infoClinic.storedTellClinica}
                                     </p>
                                     <p>
-                                        {infos[0].telefone}
-                                    </p>
-                                    <p>
-                                        {infos[0].email}
+                                        {infoClinic.storedEmailClinica}
                                     </p>
                                     <p>
                                         {infos[0].funcionamento}
@@ -65,17 +95,28 @@ export default function Perfil() {
                                 </div>
                             </div>
                             <button
-                                className="px-4 py-2 bg-primary rounded text-white font-lato text-xs self-start hover:bg-primary/90"
+                                className="px-4 py-2 bg-primary rounded text-white font-lato text-xs self-start hover:bg-primary/90 absolute bottom-0 right-0"
                             >
                                 Editar perfil
                             </button>
                         </section>
                         <div className="flex flex-col">
-                            <h2 className="mt-4 font-bold text-lg pb-2">
-                                Sobre nós
-                            </h2>
-                            <p className="text-sm font-normal ">
-                                A nossa clínica veterinária está no mercado desde 1930, oferecendo atendimento de qualidade e carinho aos animais. Somos especializados em cuidar da saúde e bem-estar de cães, gatos e outros pets. Possuímos uma equipe de profissionais altamente qualificados, que trabalham com tecnologia de ponta para diagnósticos precisos e tratamentos eficazes. Nosso objetivo é proporcionar o melhor atendimento para o seu pet, desde consultas de rotina até cirurgias e internações. Além disso, contamos com serviços de banho e tosa, pet shop e venda de produtos de qualidade para a manutenção da saúde dos animais. Venha nos visitar e conheça a nossa história de amor e dedicação aos pets.
+                            <div
+                                className="flex mt-4 justify-between w-full items-center"
+                            >
+                                <h2 className="font-bold text-lg">
+                                    Sobre nós
+                                </h2>
+                                <button
+                                    className="px-4 py-2 bg-primary rounded text-white font-lato text-xs hover:bg-primary/90"
+                                >
+                                    Editar
+                                </button>
+                            </div>
+                            <p className="text-sm font-normal">
+                                {
+                                    !infoClinic.storedDescriptionClinica || infoClinic.storedDescriptionClinica.length == 0 ? "A sua clínica ainda não possui uma descrição, crie uma agora mesmo!" : infoClinic.storedDescriptionClinica
+                                }
                             </p>
                         </div>
                         <div className="mnegative flex flex-col">
@@ -87,8 +128,16 @@ export default function Perfil() {
                             </p>
                         </div>
 
-                        <section className="mt-4 flex flex-col">
-                            <h2 className="font-bold text-lg mb-2">
+                        <section className="mt-4 flex flex-col gap-4">
+                            <h1 className="font-bold text-lg">Comentários</h1>
+                            <div
+                                className="flex flex-col gap-2"
+                            >
+                                <CommentsForClinic />
+                                <CommentsForClinic />
+                                <CommentsForClinic />
+                            </div>
+                            {/* <h2 className="font-bold text-lg mb-2">
                                 Posts
                             </h2>
                             <div className="flex flex-col gap-4">
@@ -103,7 +152,7 @@ export default function Perfil() {
                                     <input type="radio" name="anchorPost" className=" border-2 border-primary w-6 h-6 rounded-full" />
                                     <input type="radio" name="anchorPost" className="border-2 border-primary  w-6 h-6 rounded-full" />
                                 </div>
-                            </div>
+                            </div> */}
                         </section>
                     </section>
                     <aside className="flex flex-col gap-10">
@@ -123,12 +172,12 @@ export default function Perfil() {
                                 <ModalAddMedic isOpen={open} setOpen={setOpen} />
                             </div>
                         </section>
-                        <section className="w-96 bg-white px-4 py-8 rounded-2xl flex flex-col gap-5 h-max">
+                        {/* <section className="w-96 bg-white px-4 py-8 rounded-2xl flex flex-col gap-5 h-max">
                             <h1 className="font-bold text-lg">Comentários</h1>
                             <CommentsForClinic />
                             <CommentsForClinic />
                             <CommentsForClinic />
-                        </section>
+                        </section> */}
                     </aside>
                 </main>
             </section>
