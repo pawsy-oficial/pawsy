763cd08d95006efb3e658bc09413d46dfd9c9750
Fernando <fer.fernandes.moraes05@gmail.com>
2023-10-27 17:27:43 -0300

    fix: modalVaccine

--- src/components/componentsMedic/ModalVaccine/index.jsx
+++ src/components/componentsMedic/ModalVaccine/index.jsx
@@ -3,42 +3,41 @@ import { FloppyDisk, X } from "@phosphor-icons/react";
 import { useEffect, useState } from "react";
 import axios from "axios";
 import Cookies from "js-cookie";
+import { useForm } from "react-hook-form";
 
 export function ModalVaccine({ isOpenVaccine, setOpenVaccine, idPet, idClinic }) {
-	console.log(idPet, idClinic);
-
 	const [vacina, setVacina] = useState("");
 	const [retorno, setRetorno] = useState("");
-	const jwtTokenMedic = Cookies.get("jwtTokenMedic");
 	const [infoMedic, setInfoMedic] = useState([]);
-
+	
 	const tokenMedic = Cookies.get("jwtTokenMedic");
+
+	// section form
+
+	const { handleSubmit, register } = useForm({
+		mode: "onSubmit"
+	})
+
+	const onChange = (data)=>{
+		console.log(data);
+	}
+
+
+	// end section form 
+
+
+
+
+
+
+
 
 	const data = {
 		vacina: vacina,
 		id_pet: idPet,
 		id_medic: infoMedic.storedIdMedic,
-		id_clinic: "1",
+		id_clinic: idClinic,
 		dt_retorno: retorno,
-	};
-
-	const handleSubmit = async () => {
-		try {
-			axios
-				.post(`${import.meta.env.VITE_URL}/vaccine`, data, {
-					headers: {
-						Authorization: "Bearer " + jwtTokenMedic,
-					},
-				})
-				.then((e) => {
-					console.log(e);
-					setOpenVaccine(!isOpenVaccine);
-					setVacina("");
-				})
-				.catch((err) => console.log(err));
-		} catch (error) {
-			console.log("Erro ao integrar vermifugo: ");
-		}
 	};
 
 	return (
@@ -53,45 +52,48 @@ export function ModalVaccine({ isOpenVaccine, setOpenVaccine, idPet, idClinic })
 							>
 								<X size={24} color="#22937E" />
 							</button>
-							<h2 className="text-2xl font-semibold">Registrar nova dose</h2>
-							<div className="flex pt-6 flex-col gap-5">
+							<h2 
+								className="text-2xl font-semibold"	
+							>
+								Registrar nova dose
+							</h2>
+
+							<form 
+								className="flex pt-6 flex-col gap-5"
+								onSubmit={handleSubmit(onChange)}
+							>
 								<div className="flex flex-col gap-1">
 									<p className="text-xs">Proteção</p>
 									<select
-										value={vacina}
-										onChange={(e) => setVacina(e.target.value)}
 										className="bg-gray-white rounded-lg pl-2 pr-4 w-full h-8 text-xs text-[#909090] border focus:border-primary focus-visible:outline-none hover:border-primary"
+										{...register("typeVaccine")}
 									>
-										<option value="Antirrábica">Antirrábica</option>
-										<option value="Viratec 10 CVL">Viratec 10 CVL</option>
-										<option value="Giárdia">Giárdia</option>
-										<option value="Leishmaniose">Leishmaniose</option>
-										<option value="Gripe canina">Gripe canina</option>
-										<option value="Polivalente">Polivalente</option>
+										<option value="1">Antirrábica</option>
+										<option value="2">Viratec 10 CVL</option>
+										<option value="3">Giárdia</option>
+										<option value="4">Leishmaniose</option>
+										<option value="5">Gripe canina</option>
+										<option value="6">Polivalente</option>
 									</select>
 								</div>
 								<div className="flex flex-col gap-1">
 									<p className="text-xs">Data de Retorno</p>
 									<input
-										value={retorno}
-										onChange={(e) => {
-											console.log(e.target.value);
-											setRetorno(e.target.value);
-										}}
+										{...register("return")}
 										type="date"
 										className="bg-gray-white rounded-lg pl-2 pr-4 w-full h-8 text-xs text-[#909090] border focus:border-primary"
 									/>
 								</div>
-							</div>
-							<div className="flex flex-row place-content-end pt-6">
-								<button
-									onClick={handleSubmit}
-									className="flex items-center bg-[#22937E] text-white w-[7.688rem] h-8 justify-center rounded-lg gap-[10px]"
-								>
-									<FloppyDisk size={24} />
-									SALVAR
-								</button>
-							</div>
+								<div className="flex flex-row place-content-end">
+									<button
+										type="submit"
+										className="flex items-center bg-[#22937E] text-white w-[7.688rem] h-8 justify-center rounded-lg gap-[10px]"
+									>
+										<FloppyDisk size={24} />
+										SALVAR
+									</button>
+								</div>
+							</form>
 						</div>
 					</div>
 				)
