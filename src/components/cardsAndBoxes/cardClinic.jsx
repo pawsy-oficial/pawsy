c44a08e115e4db183d2059aa26b9205685fc1576
rom013 <romullomelo0130@gmail.com>
2023-10-29 02:15:45 -0300

    feat: show cards posts pageVaccines tutor

--- src/components/cardsAndBoxes/cardClinic.jsx
+++ src/components/cardsAndBoxes/cardClinic.jsx
@@ -1,21 +1,43 @@
-import znvet from "../../img/znvet.jpg";
 import { memo } from "react";
+import { useNavigate } from "react-router-dom";
 
-function CardClinic() {
+function CardClinic({ title, image, nameClinic, description, idClinic }) {
+    const navigate = useNavigate()
+
     return (
-        <div className="bg-white p-3 rounded-lg w-full lg:min-w-[256px] flex flex-col gap-4">
+        <button 
+            className="bg-white p-3 rounded-lg w-full max-w-sm lg:min-w-[256px] flex flex-col gap-4 hover:bg-emerald-100 transition-colors duration-300"
+            title={`Conhecer mais sobre a ${nameClinic}`}
+            onClick={()=>navigate("/clinica", { state: { id: idClinic } })}
+        >
             <div className="flex gap-3 items-center flex-col md:flex-row">
-                <img src={znvet} alt="znvet" className="rounded-full md:w-14 md:h-14 h-16 w-16" />
+                <img 
+                    src={`${import.meta.env.VITE_URL}/files/${image}`} 
+                    alt={nameClinic} 
+                    className="rounded-full md:w-14 md:h-14 h-16 w-16" 
+                />
                 <div className="flex flex-col gap-2 items-center md:items-start">
-                    <h3 className="font-bold text-2xl">ZNVet</h3>
-                    <span className="text-xs text-[#409E44]">Aberto</span>
+                    <h3 
+                        className="font-bold text-2xl uppercase"
+                    >
+                        {nameClinic}
+                    </h3>
+                    {/* <span className="text-xs text-[#409E44]">Aberto</span> */}
                 </div>
             </div>
-            <div className="flex flex-col gap-2">
-                <p className="pt-2 md:text-base text-xs font-semibold">Vacinação gratuita contra raiva</p>
-                <span className="text-sm">Todos os pets</span>
+            <div className="flex flex-col gap-1 w-full">
+                <p 
+                    className="w-fit md:text-base text-xs font-semibold"
+                >
+                    {title}
+                </p>
+                <span 
+                    className="text-sm text-limit text-start"
+                >
+                    {description}
+                </span>
             </div>
-        </div>
+        </button>
     );
 }
 
--- src/components/header/ProfileModal.jsx
+++ src/components/header/ProfileModal.jsx
@@ -48,7 +48,9 @@ function ProfileModal({ userType }) {
 
     return (
         <Popover.Root>
-            <Popover.Trigger className='group' aria-label="Informações do perfil">
+            <Popover.Trigger 
+                className='group' aria-label="Informações do perfil"
+            >
                 <div className='flex items-center gap-3'>
                     <div className="rounded-full w-10 h-10 overflow-hidden border-2 border-primary">
                         <img
@@ -63,14 +65,18 @@ function ProfileModal({ userType }) {
             </Popover.Trigger>
             <Popover.Portal>
                 <Popover.Content
-                    className="rounded-lg py-3 bg-white shadow-[0_10px_38px_-10px_hsla(206,22%,7%,.35),0_10px_20px_-15px_hsla(206,22%,7%,.2)]"
+                    className="rounded-lg py-3 z-[600] bg-white shadow-[0_10px_38px_-10px_hsla(206,22%,7%,.35),0_10px_20px_-15px_hsla(206,22%,7%,.2)]"
                     sideOffset={5}
                 >
                     <ul className="flex flex-col gap-3">
-                        <li className='flex gap-3 items-center px-3 py-1 cursor-pointer hover:bg-primary/10 transition-all'>
-                            <UserCircle size={24} color="#22B77E" />
-                            <span>Perfil</span>
-                        </li>
+                        {
+                            userType == "tutor" && (
+                                <li className='flex gap-3 items-center px-3 py-1 cursor-pointer hover:bg-primary/10 transition-all'>
+                                    <UserCircle size={24} color="#22B77E" />
+                                    <span>Perfil</span>
+                                </li>
+                            )
+                        }
                         <li className='flex gap-3 items-center px-3 py-1 cursor-pointer hover:bg-primary/10 transition-all'>
                             <Gear size={24} color="#22B77E" />
                             <span>Configurações</span>
