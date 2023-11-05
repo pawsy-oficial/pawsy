5029fedc057bca0017acee2d18d7e9dad26a96f7
rom013 <romullomelo0130@gmail.com>
2023-11-05 16:24:32 -0300

    refactor: initial page medic

--- src/pages/medic/medic.jsx
+++ src/pages/medic/medic.jsx
@@ -1,91 +1,69 @@
 import { HeaderMedic } from "../../components/HeaderMedic";
 import CardClinics from "../../components/componentsMedic/CardClinics/CardClinics";
-import ZnVet from "../../img/znvet.jpg"
-import randomClinic from "../../img/colors=purple.png"
 import { useEffect, useState } from "react";
 import axios from "axios";
 import Cookies from "js-cookie";
 
 export default function Medic() {
-  const date = new Date()
-  const dateHour = date.getHours()
+	const [infoMedic, setInfoMedic] = useState([]);
+	const [idMedic, setIdMedic] = useState(null)
+	const [clinicsMedic, setClinicsMedic] = useState([]);
 
-  const horarios = [8, 12, 14, 19]
+	const tokenMedic = Cookies.get("jwtTokenMedic")
 
-  const [infoMedic, setInfoMedic] = useState([]);
+	useEffect(() => {
+		axios.get(`${import.meta.env.VITE_URL}/profileMedic`, {
+			headers: {
+				Authorization: `Bearer ${tokenMedic}`
+			}
+		}).then(res => {
+			setInfoMedic(res.data)
+			setIdMedic(res.data.storedIdMedic)
+		})
 
-  const [idMedic, setIdMedic] = useState(null)
+		axios.get(`${import.meta.env.VITE_URL}/clinicsMedic`, {
+			headers: {
+				Authorization: `Bearer ${tokenMedic}`
+			}
+		}).then(res => {
+			setClinicsMedic(res.data.results)
+		})
+	}, []);
 
-  const [clinicsMedic, setClinicsMedic] = useState([]);
-
-  const [openOrClose, setOpenOrClose] = useState(false);
-
-  const tokenMedic = Cookies.get("jwtTokenMedic")
-
-  useEffect(() => {
-    const checkOpeningHours = () => {
-      const currentHour = new Date().getHours();
-      if ((currentHour >= horarios[0] && currentHour < horarios[1]) || (currentHour >= horarios[2] && currentHour < horarios[3])) {
-        setOpenOrClose(true);
-      } else {
-        setOpenOrClose(false);
-      }
-    };
-
-    axios.get(`${import.meta.env.VITE_URL}/profileMedic`, {
-      headers: {
-        Authorization: `Bearer ${tokenMedic}`
-      }
-    }).then(res =>{
-      setInfoMedic(res.data)
-      setIdMedic(res.data.storedIdMedic)
-    })
-
-    axios.get(`${import.meta.env.VITE_URL}/clinicsMedic`, {
-      headers: {
-        Authorization: `Bearer ${tokenMedic}`
-      }
-    }).then(res =>{
-      setClinicsMedic(res.data.results)
-    })
-
-    checkOpeningHours();
-    const intervalId = setInterval(checkOpeningHours, 60000);
-
-    return () => {
-      clearInterval(intervalId);
-    };
-  }, []);
-
-  return (
-    <>
-      <header>
-        <HeaderMedic />
-      </header>
-      <section className="max-w-7xl mx-auto mt-8 bg-[#F5F7FB]">
-        <h1 className="text-3xl font-semibold">
-          Olá, {infoMedic.storedNameMedic}
-        </h1>
-        <h3 className="mt-8 text-lg">
-          Clínicas onde você trabalha:
-        </h3>
-        <nav className="flex flex-wrap gap-3">
-          {
-            clinicsMedic.map((clinic, i) => {
-              return (
-                <CardClinics 
-                  key={i} 
-                  img={clinic.url_imagem} 
-                  nameClinic={clinic.nm_clinica} 
-                  openOrClose={clinic.status_loja}
-                  idClinic={clinic.id_clinica}
-                  idMedic={idMedic}
-                />
-              )
-            })
-          }
-        </nav>
-      </section>
-    </>
-  )
+	return (
+		<>
+			<header>
+				<HeaderMedic />
+			</header>
+			<section className="max-w-7xl mx-auto mt-8 bg-[#F5F7FB]">
+				<h1 className="text-3xl font-semibold capitalize">
+					Olá, {infoMedic.storedNameMedic}
+				</h1>
+				<h3 
+					className="mt-8 text-lg"
+				>
+					Clínicas onde você trabalha:
+				</h3>
+				<nav 
+					className="flex flex-wrap gap-3 mt-4"
+				>
+					{
+						clinicsMedic.length == 0
+						? <p>Você não está registrado em nenhuma clínica</p>
+						: clinicsMedic.map((clinic, i) => {
+							return (
+								<CardClinics
+									key={i}
+									img={clinic.url_imagem}
+									nameClinic={clinic.nm_clinica}
+									idClinic={clinic.id_clinica}
+									idMedic={idMedic}
+								/>
+							)
+						})
+					}
+				</nav>
+			</section>
+		</>
+	)
 }
